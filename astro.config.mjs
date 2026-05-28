975614d18b9ab16739e5b9b267c4c4ad1c6549a2
kaan <kaaan3452@gmail.com>
2026-05-28 17:58:17 +0300

    Switch to Netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: netlify(),
 });
