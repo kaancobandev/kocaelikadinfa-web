87a5c083bb4980b94ab3f86faa83635d4df88b0c
kaan <kaaan3452@gmail.com>
2026-06-08 12:58:58 +0300

    SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
 export default defineConfig({
+  site: 'https://kocaelikadinfa.com',
   output: 'server',
   adapter: netlify(),
 });
